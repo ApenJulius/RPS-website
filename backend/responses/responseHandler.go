18c638ab0fb204d81adea9fcefb64907fbb76821
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-07 13:55:23 +0100

    Can now send additional params

--- backend/responses/responseHandler.go
+++ backend/responses/responseHandler.go
@@ -5,13 +5,15 @@ import (
 	"strconv"
 )
 
-func CreateResponse(code GameStatus, message string, groupID string) (string, error) {
-	response := map[string]string{
+func CreateResponse(code GameStatus, message string, groupID string, data ...interface{}) (string, error) {
+	response := map[string]interface{}{
 		"code":    strconv.Itoa(int(code)),
 		"info":    message,
 		"groupID": groupID,
 	}
-
+	if len(data) > 0 {
+		response["data"] = data[0]
+	}
 	jsonString, err := json.Marshal(response)
 	if err != nil {
 		return "", err
