38a5ee3537d89369669728ceb51a00affd9ee43d
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-07 13:14:03 +0100

    Some routing to use params for game ids

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import GamePage from './pages/gamePage/gamePage'; // Make sure to import your GamePage component
+import LobbyPage from './pages/LobbyPage/LobbyPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LobbyPage />} />
+
+        <Route path="/:id" element={<GamePage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import GamePage from './pages/gamePage.tsx/gamePage';
+import GamePage from './pages/gamePage/gamePage';
+import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <GamePage />
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
