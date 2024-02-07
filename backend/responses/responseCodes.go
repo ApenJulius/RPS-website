210ecd3f6ba6ab587b08de78736983ebd889c2f6
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-07 13:55:41 +0100

    More response codes added. Need better system for this

--- backend/responses/responseCodes.go
+++ backend/responses/responseCodes.go
@@ -3,6 +3,9 @@ package responses
 type GameStatus int
 
 const (
-	GameFound GameStatus = 10
+	GameFound    GameStatus = 10
+	PlayerJoined GameStatus = 20
+	PlayerLeft   GameStatus = 30
+	GameStarted  GameStatus = 40
 	// other statuses...
 )
