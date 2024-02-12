c50de6ecfac154d0aa8dc8bf397923a33f24e272
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-12 14:54:39 +0100

    Moved into file for structure

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"RPS-backend/game"
+	"RPS-backend/globals"
 	"RPS-backend/paths"
-	"RPS-backend/responses"
 	"RPS-backend/structs"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -13,16 +11,20 @@ import (
 )
 
 var Clients = make(map[*websocket.Conn]bool) // connected Clients
-var broadcast = make(chan Message)           // broadcast channel
+var broadcast = make(chan structs.Message)   // broadcast channel
 
 // Define our message object
 
+func createLobbies() {
+	globals.Lobbies = make(map[string]*structs.Group)
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("../public"))
+	createLobbies()
 	http.Handle("/", fs)
-
 	http.HandleFunc("/game", paths.ConnectToGame)
-	http.handleFunc("/lobby", paths.ConnectToLobby)
+	http.HandleFunc("/lobby", paths.ConnectToLobby)
 	go handleMessages()
 
 	host := "localhost"
@@ -32,92 +34,6 @@ func main() {
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-	defer ws.Close()
-	client := &structs.Client{
-		Conn: ws,
-		Move: "",
-	}
-	groupID := r.URL.Query().Get("groupID")
-	if groupID == "" {
-		log.Println("groupID not specified") // TODO: Auto assign group
-		return
-	}
-	if _, ok := groups[groupID]; !ok {
-
-		groups[groupID] = &structs.Group{
-			Clients: make(map[*structs.Client]bool),
-			Max:     2,
-		}
-	}
-	if len(groups[groupID].Clients) >= groups[groupID].Max {
-		log.Printf("Group %s has reached its Max connections", groupID)
-		ws.WriteJSON(map[string]string{"error": "Group has reached its Max connections"})
-		return
-	}
-
-	clientAddr := ws.RemoteAddr().String()
-	groups[groupID].Clients[client] = true
-	clientCount := len(groups[groupID].Clients)
-	MaxClients := groups[groupID].Max
-	clientInfo := fmt.Sprintf("%d/%d", clientCount, MaxClients)
-	log.Printf("New client( %s ) connected to group %s : %s", clientAddr, groupID, clientInfo)
-	response, err := responses.CreateResponse(responses.GameFound, "Connected to group successfully", groupID)
-	if err != nil {
-		// handle error
-	}
-	ws.WriteMessage(websocket.TextMessage, []byte(response))
-
-	sendGroupUpdate := func() {
-		data := map[string]interface{}{
-			"current": len(groups[groupID].Clients),
-			"max":     groups[groupID].Max,
-		}
-		response, err := responses.CreateResponse(responses.PlayerJoined, "Player added", groupID, data)
-		if err != nil {
-			// handle error
-		}
-		for conn := range groups[groupID].Clients {
-			if err := conn.Conn.WriteMessage(websocket.TextMessage, []byte(response)); err != nil {
-				log.Println("write:", err)
-			}
-		}
-	}
-
-	sendGroupUpdate() // Call after client is added
-	if len(groups[groupID].Clients) == groups[groupID].Max {
-		go game.PlayGame(groups[groupID])
-	}
-	validMoves := []string{"rock", "paper", "scissors"} // temp valid move check
-
-	for {
-		var msg Message
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(groups[groupID].Clients, client)
-			sendGroupUpdate() // Call after client is removed
-			break
-		}
-		for _, move := range validMoves {
-			if msg.Move == move {
-				client.Move = msg.Move // Update the client's move
-				break
-			}
-		}
-		log.Printf("Received message from %s: %v", clientAddr, msg) // print the client's address and the received message
-		for conn := range groups[groupID].Clients {
-			if err := conn.Conn.WriteJSON(msg); err != nil {
-				log.Println("write:", err)
-			}
-		}
 	}
 }
 
