6b8cb44a1e837756efe9e343cd036d80a1cab984
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-13 12:08:05 +0100

    now gets correctly into lobbies

--- backend/paths/lobby.go
+++ backend/paths/lobby.go
@@ -4,8 +4,11 @@ import (
 	"RPS-backend/globals"
 	"RPS-backend/responses"
 	"RPS-backend/utils"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func ConnectToLobby(w http.ResponseWriter, r *http.Request) {
@@ -15,9 +18,20 @@ func ConnectToLobby(w http.ResponseWriter, r *http.Request) {
 	}
 	defer ws.Close()
 	log.Println(globals.Lobbies)
-	_, err = responses.CreateResponse(responses.LobbyConnect, "Connected to lobby", "0", {globals.Lobbies})
+	data := make(map[string]interface{})
+
+	for id, lobby := range globals.Lobbies {
+		data[id] = map[string]interface{}{
+			"clients": len(lobby.Clients),
+			"max":     lobby.Max,
+		}
+	}
+
+	response, err := responses.CreateResponse(responses.LobbyConnect, "Connected to lobby", "0", data)
 	if err != nil {
 		// handle error
+		fmt.Println(err)
 	}
+	ws.WriteMessage(websocket.TextMessage, []byte(response))
 
 }
