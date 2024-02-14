caafd86aa9ed1cd5070aac58b13581230d448169
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-14 14:48:31 +0100

    Mainstains connection and notifies all users "InBrowser"

--- backend/paths/lobby.go
+++ backend/paths/lobby.go
@@ -1,8 +1,7 @@
 package paths
 
 import (
-	"RPS-backend/globals"
-	"RPS-backend/responses"
+	"RPS-backend/structs"
 	"RPS-backend/utils"
 	"fmt"
 	"log"
@@ -17,21 +16,19 @@ func ConnectToLobby(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	defer ws.Close()
-	log.Println(globals.Lobbies)
-	data := make(map[string]interface{})
-
-	for id, lobby := range globals.Lobbies {
-		data[id] = map[string]interface{}{
-			"clients": len(lobby.Clients),
-			"max":     lobby.Max,
-		}
-	}
-
-	response, err := responses.CreateResponse(responses.LobbyConnect, "Connected to lobby", "0", data)
+	response, err := utils.LobbyListUpdate()
 	if err != nil {
-		// handle error
 		fmt.Println(err)
 	}
 	ws.WriteMessage(websocket.TextMessage, []byte(response))
+
+	for {
+		var msg structs.Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			break
+		}
+	}
 
 }
