0ca1039fd3e44504b062a2c7fabb1b8b7e0cd254
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-02-17 19:51:10 +0100

    Now adds and removes users from InBrowser slice

--- backend/paths/lobby.go
+++ backend/paths/lobby.go
@@ -1,14 +1,25 @@
 package paths
 
 import (
+	"RPS-backend/globals"
 	"RPS-backend/structs"
 	"RPS-backend/utils"
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
+
+func fastRemove(slice []*structs.Client, i int) []*structs.Client {
+	slice[i] = slice[len(slice)-1]
+	return slice[:len(slice)-1]
+}
+func findClientIndex(client *structs.Client, clients []*structs.Client) int {
+	for i, c := range clients {
+		if c == client {
+			return i
+		}
+	}
+	return -1 // return -1 if the client is not found
+}
 
 func ConnectToLobby(w http.ResponseWriter, r *http.Request) {
 	ws, err := utils.Upgrader.Upgrade(w, r, nil)
@@ -16,17 +27,22 @@ func ConnectToLobby(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	defer ws.Close()
-	response, err := utils.LobbyListUpdate()
-	if err != nil {
-		fmt.Println(err)
+	client := &structs.Client{
+		Conn: ws,
+		Move: "",
 	}
-	ws.WriteMessage(websocket.TextMessage, []byte(response))
+	globals.InBrowser = append(globals.InBrowser, client)
+	utils.LobbyListUpdate()
 
 	for {
 		var msg structs.Message
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
+			clientIndex := findClientIndex(client, globals.InBrowser)
+			if clientIndex != -1 { // make sure the client is in the slice
+				globals.InBrowser = fastRemove(globals.InBrowser, clientIndex)
+			}
 			break
 		}
 	}
