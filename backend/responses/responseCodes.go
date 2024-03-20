18b28a860ad1a74f864bcf92fe4883e9c511dd1b
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-03-20 19:51:01 +0100

    Add new game status codes

--- backend/responses/responseCodes.go
+++ backend/responses/responseCodes.go
@@ -8,6 +8,8 @@ const (
 	PlayerLeft      GameStatus = 30
 	GameStarted     GameStatus = 40
 	GameCountdown   GameStatus = 41
+	GameOver        GameStatus = 42
+	ScoreUpdate     GameStatus = 43
 	LobbyConnect    GameStatus = 50
 	LobbyListUpdate GameStatus = 51
 	// other statuses...
