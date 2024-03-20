05328858534ef7e6a4b391230062b1dca5b4a3ba
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-03-20 19:51:49 +0100

    Add game over responses and score updates. Not my proudest update

--- backend/game/gameHandler.go
+++ backend/game/gameHandler.go
@@ -47,20 +47,51 @@ func compareMoves(group *structs.Group, groupID string) { // 2player max
 		moves = append(moves, client.Move)
 		clients = append(clients, client)
 	}
-
+	WinRes, err := responses.CreateResponse(responses.GameOver, fmt.Sprintf("You win!"), groupID)
+	if err != nil {
+		// handle error
+	}
+	LoseRes, err := responses.CreateResponse(responses.GameOver, fmt.Sprintf("You lose!"), groupID)
+	if err != nil {
+		// handle error
+	}
+	DrawRes, err := responses.CreateResponse(responses.GameOver, fmt.Sprintf("It's a draw!"), groupID)
+	if err != nil {
+		// handle error
+	}
 	if moves[0] == moves[1] {
 		fmt.Println("It's a draw!")
-		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte("It's a draw!"))
-		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte("It's a draw!"))
+		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte(DrawRes))
+		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte(DrawRes))
 	} else if (moves[0] == "rock" && moves[1] == "scissors") ||
 		(moves[0] == "scissors" && moves[1] == "paper") ||
-		(moves[0] == "paper" && moves[1] == "rock") {
+		(moves[0] == "paper" && moves[1] == "rock") ||
+		(moves[0] != "" && moves[1] == "") {
 		fmt.Println("Client", clients[0], "wins!")
-		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte("You win!"))
-		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte("You lose!"))
+		clients[0].Score++
+		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte(WinRes))
+		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte(LoseRes))
 	} else {
+		clients[1].Score++
 		fmt.Println("Client", clients[1], "wins!")
-		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte("You lose!"))
-		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte("You win!"))
+		clients[0].Conn.WriteMessage(websocket.TextMessage, []byte(LoseRes))
+		clients[1].Conn.WriteMessage(websocket.TextMessage, []byte(WinRes))
 	}
+	scoreUpdate1, err := responses.CreateResponse(responses.ScoreUpdate, "", groupID, struct {
+		Score1 int
+		Score2 int
+	}{
+		Score1: clients[0].Score,
+		Score2: clients[1].Score,
+	})
+	clients[0].Conn.WriteMessage(websocket.TextMessage, []byte(scoreUpdate1))
+	scoreUpdate2, err := responses.CreateResponse(responses.ScoreUpdate, "", groupID, struct {
+		Score1 int
+		Score2 int
+	}{
+		Score1: clients[1].Score,
+		Score2: clients[0].Score,
+	})
+	clients[1].Conn.WriteMessage(websocket.TextMessage, []byte(scoreUpdate2))
+
 }
